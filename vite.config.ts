032ee7813ffa9da729ff3b46e49cd61dc0d68803
ElhoubeBrahim <brahim.elhoube@gmail.com>
2025-02-08 20:59:09 +0100

    setup tailwind and chadcn vue

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
@@ -41,11 +48,11 @@ export default defineConfig({
         target: 'http://localhost:8888',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/jupyter/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
             console.log('proxy error', err)
           })
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
+          proxy.on('proxyReq', (proxyReq, req) => {
             if (req.headers.authorization) {
               proxyReq.setHeader('Authorization', req.headers.authorization)
             }
